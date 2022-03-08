0a3ac90550b9ac56509851ca3fc65a11b011a87b
Jhonny07 <jhonnyjacinto09@icloud.com>
2022-03-08 17:58:29 +0000

    abstrats

---
+++ Aguia.java
@@ -0,0 +1,11 @@
+public class Aguia extends Ave {
+    
+    @Override
+    public String nadar() {
+        return "nada mal";
+    }
+    @Override
+    public String voar() {
+        return "voa bem!";
+    }
+}
---
+++ Principal.java
@@ -0,0 +1,18 @@
+public class Principal {
+    public static void main(String [] args) {
+
+
+        Papagaio p=new Papagaio();
+        Aguia a=new Aguia();
+
+        a.setNome("Corinthians");
+        System.out.println(a.getNome());
+        System.out.println(a.voar());
+        System.out.println(a.nadar());
+
+        p.setNome("Loro");
+        System.out.println(p.getNome());
+        System.out.println(p.voar());
+        System.out.println(p.nadar());
+    }
+}
